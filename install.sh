417d86052964d8627550050ce2df5f45286afafe
Nguyen Van Dat <phongdatgl@gmail.com>
2023-04-10 14:43:33 +0700

    Adding user/pass random or not

--- install.sh
+++ install.sh
@@ -1,111 +1,5 @@
 #!/bin/sh
 random() {
-	tr </dev/urandom -dc A-Za-z0-9 | head -c5
-	echo
-}
-
-array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
-main_interface=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
-
-gen64() {
-	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
-}
-gen16() {
-    ip16() {
-        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-    }
-    echo "$1:$(ip16)"
-}
-install_3proxy() {
-    echo "Installing 3proxy"
-    mkdir -p /3proxy
-    cd /3proxy
-    URL="https://github.com/z3APA3A/3proxy/archive/0.9.3.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.9.3
-    make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    mv /3proxy/3proxy-0.9.3/bin/3proxy /usr/local/etc/3proxy/bin/
-    wget https://raw.githubusercontent.com/phongdatgl/proxyserver/main/3proxy.service-Centos7 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
-    cp /3proxy/3proxy-0.9.3/scripts/3proxy.service2 /usr/lib/systemd/system/3proxy.service
-    systemctl link /usr/lib/systemd/system/3proxy.service
-    systemctl daemon-reload
-    echo "* hard nofile 999999" >>  /etc/security/limits.conf
-    echo "* soft nofile 999999" >>  /etc/security/limits.conf
-    echo "net.ipv6.conf.$main_interface.proxy_ndp=1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
-    echo "net.ipv6.ip_nonlocal_bind = 1" >> /etc/sysctl.conf
-    sysctl -p
-    systemctl stop firewalld
-    systemctl disable firewalld
-
-    cd $WORKDIR
-}
-
-gen_3proxy() {
-    cat <<EOF
-daemon
-maxconn 2000
-nserver 1.1.1.1
-nserver 8.8.4.4
-nserver 2001:4860:4860::8888
-nserver 2001:4860:4860::8844
-nscache 65536
-timeouts 1 5 30 60 180 1800 15 60
-setgid 65535
-setuid 65535
-stacksize 6291456 
-flush
-auth strong
-
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' ${WORKDATA})
-EOF
-}
-
-gen_proxy_file_for_user() {
-    cat >$PROXYDIR <<EOF
-$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
-EOF
-}
-
-gen_data() {
-    seq $FIRST_PORT $LAST_PORT | while read port; do
-        if [[ $SUBNETMASK -eq 64 ]]
-        then
-            echo "$(random)/$(random)/$IP4/$port/$(gen64 $EXTERNAL_IP)"
-        else
-            echo "$(random)/$(random)/$IP4/$port/$(gen16 $EXTERNAL_IP)"
-        fi        
-    done
-}
-
-gen_iptables() {
-    cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
-EOF
-}
-
-gen_ifconfig() {
-    cat <<EOF
-$(awk -F "/" '{print "ifconfig '$main_interface' inet6 add " $5 "/'$SUBNETMASK'"}' ${WORKDATA})
-EOF
-}
-echo "install wget iptables gcc net-tools libarchive bsdtar zip make"
-yum -y install wget iptables gcc net-tools libarchive bsdtar zip make
-
-
-IP4=$(curl -4 -s icanhazip.com)#!/bin/sh
-random() {
     tr </dev/urandom -dc A-Za-z0-9 | head -c5
     echo
 }
@@ -186,11 +80,21 @@ EOF
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
+        if [[ "$USERN" == "" ]]; then
+            puser=$USERN
+        else
+            puser=$(random)
+        fi
+        if [[ "$PASSW" == "" ]]; then
+            ppass=$PASSW
+        else
+            ppass=$(random)
+        fi
         if [[ $SUBNETMASK -eq 64 ]]
         then
-            echo "$(random)/$(random)/$IP4/$port/$(gen64 $EXTERNAL_IP)"
+            echo "$puser/$ppass/$IP4/$port/$(gen64 $EXTERNAL_IP)"
         else
-            echo "$(random)/$(random)/$IP4/$port/$(gen16 $EXTERNAL_IP)"
+            echo "$puser/$ppass/$IP4/$port/$(gen16 $EXTERNAL_IP)"
         fi        
     done
 }
@@ -231,14 +135,22 @@ read SUBNETMASK
 echo "Enter external ipv6 (None if ip6): "
 read EXTERNAL
 
-if [[ EXTERNAL -eq "" ]]
+echo "Enter Proxy Username (None = random): "
+read USERN
+
+echo "Enter Proxy Password (None = random): "
+read PASSW
+
+echo "Install Proxy."
+
+if [[ "$EXTERNAL" == "" ]]
 then
     EXTERNAL_IP=$IP6
 else
     EXTERNAL_IP=$EXTERNAL
 fi
 
-FIRST_PORT=10000
+FIRST_PORT=11000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
 gen_data >$WORKDIR/data.txt
