d0e271338ea52e0cc1c3ec8f24b30ef70defa46f
Nguyen Van Dat <phongdatgl@gmail.com>
2023-05-20 14:50:46 +0700

    add cronjob

--- gen_more.sh
+++ gen_more.sh
@@ -19,34 +19,6 @@ gen16() {
     }
     echo "$1:$(ip16)"
 }
-install_3proxy() {
-    echo "Installing 3proxy"
-    mkdir -p /3proxy
-    cd /3proxy
-    URL="https://github.com/z3APA3A/3proxy/archive/0.9.3.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.9.3
-    make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    mv /3proxy/3proxy-0.9.3/bin/3proxy /usr/local/etc/3proxy/bin/
-    wget https://raw.githubusercontent.com/phongdatgl/proxyserver/main/3proxy.service-Centos7 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
-    cp /3proxy/3proxy-0.9.3/scripts/3proxy.service2 /usr/lib/systemd/system/3proxy.service
-    systemctl link /usr/lib/systemd/system/3proxy.service
-    systemctl daemon-reload
-    echo "* hard nofile 999999" >>  /etc/security/limits.conf
-    echo "* soft nofile 999999" >>  /etc/security/limits.conf
-    echo "net.ipv6.conf.$main_interface.proxy_ndp=1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
-    echo "net.ipv6.ip_nonlocal_bind = 1" >> /etc/sysctl.conf
-    sysctl -p
-    systemctl stop firewalld
-    systemctl disable firewalld
-
-    cd $WORKDIR
-}
-
 gen_3proxy() {
     cat <<EOF
 daemon
@@ -62,16 +34,13 @@ setuid 65535
 stacksize 6291456 
 flush
 auth strong
-
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-
 $(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
 }
-
 gen_proxy_file_for_user() {
     cat >$PROXYDIR <<EOF
 $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
@@ -110,16 +79,15 @@ gen_ifconfig() {
 $(awk -F "/" '{print "ifconfig '$main_interface' inet6 add " $5 "/'$SUBNETMASK'"}' ${WORKDATA})
 EOF
 }
-echo "install wget iptables gcc net-tools libarchive bsdtar zip make"
-yum -y install wget iptables gcc net-tools libarchive bsdtar zip make
-
-install_3proxy
-
 echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
 PROXYDIR="${WORKDIR}/proxy.txt"
-mkdir $WORKDIR && cd $_
+
+# unlink $WORKDATA
+# unlink $PROXYDIR
+# unlink $WORKDIR/boot_iptables.sh
+# unlink $WORKDIR/boot_ifconfig.sh
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
@@ -150,23 +118,22 @@ else
     EXTERNAL_IP=$EXTERNAL
 fi
 
-FIRST_PORT=11000
+echo "FIRST PORT: "
+read fp
+
+FIRST_PORT=21000
+if [[ "$fp" != "" ]]; then
+    FIRST_PORT=$fp
+fi
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
-gen_data >$WORKDIR/data.txt
+gen_data >> $WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
 chmod +x $WORKDIR/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
-cat >>/etc/rc.local <<EOF
-/sbin/service network restart
-bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
-ulimit -n 65535
-/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
-EOF
 
 bash /etc/rc.local
 
@@ -174,10 +141,3 @@ gen_proxy_file_for_user
 
 echo "Proxy generate done "
 echo $PROXYDIR
-
-echo "Write crontab"
-
-crontab -l > mycron
-echo "*/2 * * * * /usr/bin/bash /home/proxy-installer/boot_ifconfig.sh" >> mycron
-crontab mycron
-rm mycron
